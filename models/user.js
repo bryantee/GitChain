e884f89d8196932beedcf6c01bfc27ef8c1e9d47
bryantee <bryantee@gmail.com>
2016-11-28 15:55:29 -0700

    Add User model for mongoose

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  avatar: { type: String },
+  currentGoal: { type: String },
+  currentCommitStreakDays: { type: Number },
+  commitsToday: { type: Number },
+  highStreak: { type: Number },
+  lastCommit: { type: Date }
+});
+
+// TODO: Set instance method for user to check if commit hasn't been made, signaling broken streak
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = Item;
