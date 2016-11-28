0dd8708c15644a5c92f18cc4f1fe371d87be6dce
bryantee <bryantee@gmail.com>
2016-11-28 16:06:18 -0700

    Setup db connection in server.js and require User model

--- models/user.js
+++ models/user.js
@@ -14,4 +14,4 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', UserSchema);
 
-module.exports = Item;
+module.exports = User;
--- server.js
+++ server.js
@@ -7,10 +7,33 @@ const config = require('./config');
 const app = express();
 app.use(express.static('build'));
 
+// Get model
+const User = require('./models/user');
 
 
-app.listen(config.PORT, () => {
-  console.log(`Listening on port ${config.PORT}`);
-});
+
+// Startup server
+const runServer = function(callback) {
+  mongoose.connect(config.DATABASE_URL, err => {
+    if (err && callback) {
+      return callback(err);
+    }
+
+    app.listen(config.PORT, () => {
+      console.log(`Listening on port ${config.PORT}`);
+      if (callback) {
+        callback();
+      }
+    });
+  });
+};
+
+if (require.main === module) {
+  runServer(err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
 
 module.exports = app;
