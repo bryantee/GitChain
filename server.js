c85339d8f323a4be295d41d6b27cbf8c492e6b2e
bryantee <bryantee@gmail.com>
2016-11-28 18:19:56 -0700

    Setup db users seed and tear down for tests

--- server.js
+++ server.js
@@ -92,3 +92,4 @@ if (require.main === module) {
 }
 
 module.exports = app;
+module.exports.runServer = runServer;
--- test/test-server.js
+++ test/test-server.js
@@ -1,6 +1,9 @@
 'use strict';
 
+global.DATABASE_URL = 'mongodb://localhost/gitchain-test';
+
 const server = require('../server');
+const User = require('../models/user');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
@@ -9,7 +12,40 @@ const app = server.app;
 
 chai.use(chaiHttp);
 
-describe('Server', () => {
+describe('GitChain API', () => {
+  // seed db
+  before( done => {
+    server.runServer( () => {
+      User.create(
+        {
+          username: 'jason-voorhees',
+          avatar: 'https://avatars.githubusercontent.com/u/10674447?v=3',
+          currentGoal: 'MURDERRR.',
+          currentCommitStreakDays: 5,
+          commitsToday: 1,
+          highStreak: 15
+        },
+        {
+          username: 'freddy-krueger',
+          avatar: 'https://avatars.githubusercontent.com/u/10674447?v=3',
+          currentGoal: 'Slice children faces off',
+          currentCommitStreakDays: 10,
+          commitsToday: 7,
+          highStreak: 27
+        }, function() {
+            done();
+        });
+    });
+  });
+
+  // tear down db
+  after( done => {
+    User.remove( () => {
+      done();
+    });
+  });
+
+  // start actual tests
   it('should return 200 status and html on get "/"', done => {
     chai.request(server)
       .get('/')
